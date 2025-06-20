5a93cfd6753a2437c3c5b5ff7c5657de1dc5ebae
Brayan Triana <desarrollo2@caudata.com.co>
2025-06-19 21:37:00 -0500

    Creando sheduler de prueba para testear funcionamiento de cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,6 +24,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->call(function () {
+            \Log::info('Scheduler está funcionando: ' . now());
+        })->everyMinute();
         $schedule->command('alegra:update')->everyThreeHours();
         $schedule->command('payments:update')->hourly();
         $schedule->command('email:ordersend')->hourly();
